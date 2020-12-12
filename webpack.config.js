55082fde912ede458259d3807dafd1ce1cd04725
Axel Duret <axelfaya@gmail.com>
2020-12-12 21:23:34 +0100

    updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/jsmain.js',
+  entry: './src/js/main.js',
   output: {
     path: __dirname,
     filename: './dist/js/bundle.js'
